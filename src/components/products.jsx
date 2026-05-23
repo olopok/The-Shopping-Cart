b6119cb8d05eb513afb6455feac79e8c5d9e2b33
olopok <pabisa.pb@gmail.com>
2026-05-23 20:12:40 +0100

    Add quantity input with plus and minus btn, and Add to chart button.

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -3,14 +3,22 @@ import styles from "../css/cards.module.css";
 
 export default function Prod() {
   const [products, setProducts] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+
+  function itemsQuantity(product, quantity) {
+    const newProduct = { ...product, quantity };
+    return newProduct;
+  }
+
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
+      .then((data) => setProducts(data.map((p) => itemsQuantity(p, quantity))));
+  }, [quantity]);
+
   const prodList = products.map((product) => (
-    <section className={styles.mainSection}>
-      <div key={product.id} className={styles.card}>
+    <section key={product.id} className={styles.mainSection}>
+      <div className={styles.card}>
         <div className={styles.imageWrapper}>
           <img
             src={product.image}
@@ -29,8 +37,17 @@ export default function Prod() {
         <p>
           <b>Category:</b> {product.category}
         </p>
+        <div className={styles.itemsNumber}>
+          <b>Quantity:</b>
+          <button id={styles.decrement}>-</button>
+          <input type="text" defaultValue={product.quantity} />
+          <button id={styles.increment}>+</button>
+        </div>
+        <button id={styles.addToChart}>Add to chart</button>
       </div>
     </section>
   ));
+  console.log(products);
+
   return <div className={styles.cardsContainer}>{prodList}</div>;
 }
